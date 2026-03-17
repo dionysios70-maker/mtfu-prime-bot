9541e8629fed4d68ad1d15504b10f89d1b2c89f4
dionysios70-maker <dionysios70@gmail.com>
2026-03-17 22:18:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import { initDB } from "./database.js";
 import { registerPrime } from "./commands/prime.js";
 import { startExpiryCheck } from "./systems/expiryCheck.js";
 import { registerSeason } from "./commands/season.js";
+import { primeCommand, registerPrime } from "./commands/prime.js";
+import { seasonCommand, registerSeason } from "./commands/season.js";
+import { REST, Routes } from "discord.js";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
@@ -15,6 +18,18 @@ const token = process.env.BOT_TOKEN;
 await initDB();
 
 client.once("clientReady", async () => {
+
+  const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
+
+  await rest.put(
+    Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID),
+    {
+      body: [
+        primeCommand.toJSON(),
+        seasonCommand.toJSON()
+      ]
+    }
+  );
 
   console.log(`Logged in as ${client.user.tag}`);
 
