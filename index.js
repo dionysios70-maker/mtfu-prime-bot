f7ce9c90ba52a8ca440dcc947ec96e1ef7e407c7
dionysios70-maker <dionysios70@gmail.com>
2026-03-18 19:04:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 
 import express from "express";
-import { Client, GatewayIntentBits } from "discord.js";
-import { initDB } from "./database.js";
-import { registerPrime } from "./commands/prime.js";
-import { startExpiryCheck } from "./systems/expiryCheck.js";
-import { registerSeason } from "./commands/season.js";
-import { primeCommand, registerPrime } from "./commands/prime.js";
-import { seasonCommand, registerSeason } from "./commands/season.js";
-import { REST, Routes } from "discord.js";
+import { Client, GatewayIntentBits, REST, Routes } from "discord.js";
+
+import { registerPrime, primeCommand } from "./commands/prime.js";
+import { registerSeason, seasonCommand } from "./commands/season.js";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
@@ -19,22 +15,24 @@ await initDB();
 
 client.once("clientReady", async () => {
 
-  const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
 
-  await rest.put(
-    Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID),
-    {
-      body: [
-        primeCommand.toJSON(),
-        seasonCommand.toJSON()
-      ]
-    }
-  );
+await rest.put(
+  Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID),
+  {
+    body: [
+      primeCommand.toJSON(),
+      seasonCommand.toJSON()
+    ]
+  }
+);
+
+await registerPrime(client);
+await registerSeason(client);
 
   console.log(`Logged in as ${client.user.tag}`);
 
-  await registerPrime(client);
-  await registerSeason(client);
+
 
   startExpiryCheck(client);
 
